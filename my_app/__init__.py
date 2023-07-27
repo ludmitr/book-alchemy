12168865b9501940f82b5071e39211cffdc9f19d
Dmitriy Lubchenko <ludmitr87@gmail.com>
2023-07-27 18:14:58 +0300

    added style

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 import config
 
 db = SQLAlchemy()
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
+
+app.config['SQLALCHEMY_DATABASE_URI'] = config.get_absolute_db_uri()
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.template_folder = config.abs_path_templates_folder()
+app.static_folder = config.abs_path_static_folder()
+
 db.init_app(app)
 
 from my_app import routes
