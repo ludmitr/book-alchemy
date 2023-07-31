157ff3a2471855e166782b9b27aa7ec2d931b1e6
Dmitriy Lubchenko <ludmitr87@gmail.com>
2023-07-31 03:24:30 +0300

    updated js href fetch

--- static/home.js
+++ static/home.js
@@ -1,10 +1,13 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     let sortSelect = document.getElementById('sort');
+    let resetBtn = document.getElementById('resetBtn');
 
     sortSelect.addEventListener('change', function() {
         if (this.value) {
-            let currentUrl = window.location.href.split('?')[0];
-            window.location = currentUrl + "?sort=" + this.value;
+            // Getting href from Reset button
+            let resetHref = resetBtn.getAttribute('href');
+            // Update the location to the Reset button's href with the sort value
+            window.location = resetHref + "?sort=" + this.value;
         }
     });
 });
